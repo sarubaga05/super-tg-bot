842092c235eefb5fa6e609226cab0203577a6758
Sarubaga <sarubaga05@gmail.com>
2022-09-07 21:41:23 +0700

    Main files created, start_help and values handlers created

---
+++ app.py
@@ -0,0 +1,31 @@
+import requests
+import telebot
+import json
+from tkn import TOKEN
+from exten import *
+
+bot = telebot.TeleBot(TOKEN)
+currency = {
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB'
+}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start_help(message):
+    text = 'Для перевода одной валюты в другую введите команду в следующем виде: \n\
+<имя валюты которую конвертируем> <имя валюты в которую конвертируем> <количество первой валюты>\n\
+Для вывода информации о валютах введите команду /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message):
+    text = 'Список валют\n ----------'
+    for k in currency.keys():
+        text = '\n'.join((text, k, ))
+    bot.reply_to(message, text)
+
+
+bot.polling(none_stop=True)
