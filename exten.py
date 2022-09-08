77c20740bc778a99165c3219e11f99169687bf16
Sarubaga <sarubaga05@gmail.com>
2022-09-08 12:24:59 +0700

    Finished get_price method, added exception class

--- exten.py
+++ exten.py
@@ -1,6 +1,36 @@
+import requests
+import json
+from tkn import currency
+
+
+class ConvException(Exception):
+    pass
+
+
 class GetCurrency:
 
     @staticmethod
-    def get_price(base, qoute, amount):
-        pass
+    def get_price(base: str, quote: str, amount: str):
 
+        if base == quote:
+            raise ConvException('Попытка перевода одинаковых валют.')
+
+        try:
+            base_curr = currency[base]
+        except KeyError:
+            raise ConvException(f'Ошибка обработки валюты "{base}"')
+
+        try:
+            quote_curr = currency[quote]
+        except KeyError:
+            raise ConvException(f'Ошибка обработки валюты "{quote}"')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvException(f'Ошибка обработки параметра количества валюты "{amount}"')
+
+        r1 = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_curr}&tsyms={quote_curr}')
+        res = json.loads(r1.content)[quote_curr] * amount
+
+        return res
